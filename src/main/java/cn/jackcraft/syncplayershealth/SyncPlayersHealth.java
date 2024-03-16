0ccac81d085c188737c5f2f25344d45965f13c20
Jack Lei <3486688394@qq.com>
2024-03-16 16:01:36 +0800

    feat: 实现同步血量方法

--- src/main/java/cn/jackcraft/syncplayershealth/SyncPlayersHealth.java
+++ src/main/java/cn/jackcraft/syncplayershealth/SyncPlayersHealth.java
@@ -1,5 +1,7 @@
 package cn.jackcraft.syncplayershealth;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SyncPlayersHealth extends JavaPlugin {
@@ -8,5 +10,17 @@ public final class SyncPlayersHealth extends JavaPlugin {
     }
     @Override
     public void onDisable() {
+    }
+
+    public static void syncHealth(Player player, double health) {
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            if (!onlinePlayer.equals(player) && onlinePlayer.isValid()) {
+                if (health < 0) {
+                    onlinePlayer.setHealth(0);
+                } else {
+                    onlinePlayer.setHealth(health);
+                }
+            }
+        }
     }
 }
